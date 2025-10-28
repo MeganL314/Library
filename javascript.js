1db1dcdcd7bd46bbeb94ccb92b69fd577157da38
Megan Lynch <megantherese314@gmail.com>
2025-10-28 10:00:13 -0400

    Begin building the card container loop

--- javascript.js
+++ javascript.js
@@ -30,18 +30,55 @@ function addBookToLibrary(title, author, pages, read_status, bookArr) {
 }
 
 
-
-
-
-
-
 const andThen = addBookToLibrary('And Then There Were None', 
-    'Agatha Christie',
-    272, true, myLibrary)
+    'Agatha Christie', 272, true, myLibrary)
 
 const darkPlaces = addBookToLibrary('Dark Places', 
-    'Gillian Flynn',
-    349, true, myLibrary)
+    'Gillian Flynn', 349, true, myLibrary)
 
+const readyPlayer = addBookToLibrary('Ready Player One', 
+    'Ernest Cline', 480, true, myLibrary)
+
+const Circe = addBookToLibrary('Circe', 
+    'Madeline Miller', 393, true, myLibrary)
 
 console.log(myLibrary)
+
+
+
+// Create a grid of 'cards' displaying each book
+
+function createGrid(size) {
+
+  const container = document.querySelector("#container");
+  
+  // create a 16x16 square of divs
+  const widthHeight = 800 / size + "px";
+
+  for (let i=1; i<=size; i++ ){
+      const row = document.createElement("div");
+      row.classList.add("row");
+      for (let j=1; j<=size; j++ ){
+          const grid = document.createElement("div");
+          grid.classList.add("grid");
+          row.appendChild(grid);
+          grid.style.width = widthHeight;
+          grid.style.height = widthHeight;
+      }
+      container.appendChild(row);
+  }
+
+  const myGrid = document.querySelectorAll('.grid');
+
+  myGrid.forEach((square) => {
+    // and for each one we add a 'click' listener
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = 'lightblue';
+    });
+  });
+
+}
+
+
+
+
