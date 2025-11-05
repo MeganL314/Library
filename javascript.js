83b5da2cc1fe9b455d652aae556dbd7e63747064
Megan Lynch <megantherese314@gmail.com>
2025-11-05 10:34:33 -0500

    Add text to html

--- javascript.js
+++ javascript.js
@@ -21,7 +21,7 @@ function Book(title, author, pages, read_status) {
     this.pages = pages;
     this.read_status = read_status;
     this.info = function() {
-        if (this.read_status = true){
+        if (this.read_status = Read){
             message = "read";
         } else {
             message = "not read yet";
@@ -42,16 +42,16 @@ function addBookToLibrary(title, author, pages, read_status, bookArr) {
 
 
 const andThen = addBookToLibrary('And Then There Were None', 
-    'Agatha Christie', 272, true, myLibrary)
+    'Agatha Christie', 272, 'Read', myLibrary)
 
 const darkPlaces = addBookToLibrary('Dark Places', 
-    'Gillian Flynn', 349, true, myLibrary)
+    'Gillian Flynn', 349, 'Read', myLibrary)
 
 const readyPlayer = addBookToLibrary('Ready Player One', 
-    'Ernest Cline', 480, true, myLibrary)
+    'Ernest Cline', 480, 'Read', myLibrary)
 
 const Circe = addBookToLibrary('Circe', 
-    'Madeline Miller', 393, true, myLibrary)
+    'Madeline Miller', 393, 'Read', myLibrary)
 
 console.log(myLibrary)
 console.log(myLibrary.length)
@@ -67,10 +67,10 @@ function createGrid(size) {
 
   for (let i=1; i<=size; i++ ){
       const row = document.createElement("div");
-      row.classList.add("row");
+      row.classList.add("column");
       for (let j=1; j<=size; j++ ){
           const grid = document.createElement("div");
-          grid.classList.add("grid");
+          grid.classList.add("square");
           row.appendChild(grid);
           grid.style.width = widthHeight;
           grid.style.height = widthHeight;
@@ -80,8 +80,34 @@ function createGrid(size) {
 
 }
 
+
+
+// Function to add text to HTML
+function add_books(myLibrary) {
+  
+  const myGrid = document.querySelectorAll('.square');
+
+  for (let i =0; i < myLibrary.length; i++){
+    // need a nested loop through myGrid
+     // for square in myGrid:
+    console.log(myLibrary[i].title)
+
+     const bookHTML = `
+        <div class="title">Title: "${myLibrary[i].title}" </div>
+        <div class="author">Author: ${myLibrary[i].author} </div>
+        <div class="pages">Pages: ${myLibrary[i].pages} </div>
+        <div class="status">Status: ${myLibrary[i].read_status} </div>
+        <button class="read_status">Mark as read </button>
+        <button class="delete">Delete </button>
+        `;
+
+    myGrid[i].innerHTML = bookHTML;
+    }
+
+}
+
 const sqr_root = Math.sqrt(myLibrary.length);
 const round_sqrt = Math.ceil(sqr_root);
 createGrid(round_sqrt)
-
+add_books(myLibrary)
 
